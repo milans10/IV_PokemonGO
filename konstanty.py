44436d4de5c4244db5b210024041cbaee2c293c6
milans10 <54938034+milans10@users.noreply.github.com>
2026-04-10 08:12:42 +0200

    úprava konstanty.py pro nový testovací telefon

--- konstanty.py
+++ konstanty.py
@@ -1,5 +1,4 @@
 #  Copyright (c) 2025. Created by Milan Svarc
 
-#PHONE = "0238f5b90005" # run cmd "adb devices" to get a number
-PHONE = "2A311FDH200AS1" # run cmd "adb devices" to get a number
+PHONE = "0238f5b90005" # run cmd "adb devices" to get a number
 ADB = "adb -s " + PHONE + " shell input "
