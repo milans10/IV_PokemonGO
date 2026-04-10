f3aa9564ef2caab1ec6a3668ce973cde7e0b9478
milans10 <54938034+milans10@users.noreply.github.com>
2026-04-10 13:34:41 +0200

    Oprava

--- konstanty.py
+++ konstanty.py
@@ -1,4 +1,4 @@
 #  Copyright (c) 2025. Created by Milan Svarc
 
-PHONE = "0238f5b90005" # run cmd "adb devices" to get a number
+PHONE = "xxxx" # run cmd "adb devices" to get a number
 ADB = "adb -s " + PHONE + " shell input "
